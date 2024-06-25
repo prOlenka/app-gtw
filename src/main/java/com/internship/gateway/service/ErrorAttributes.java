9ca7746328709931afcd671ed17537761d725119
Pronina <olia.prosha@gmail.com>
2024-06-25 10:52:13 +0300

    Cleaner code

--- src/main/java/com/internship/gateway/service/ErrorAttributes.java
+++ src/main/java/com/internship/gateway/service/ErrorAttributes.java
@@ -1,15 +1,14 @@
 package com.internship.gateway.service;
 
-//import org.springframework.boot.web.error.ErrorAttributeOptions;
-//import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-//import org.springframework.web.context.request.WebRequest;
-//import org.springframework.web.reactive.function.server.ServerRequest;
-//
-//import java.util.Map;
-//
-//public class ErrorAttributes extends DefaultErrorAttributes {
-//
-//    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options){
-//        return super.getErrorAttributes((WebRequest) request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE));
-//    }
-//}
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import java.util.Map;
+
+public class ErrorAttributes extends DefaultErrorAttributes {
+
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options){
+        return super.getErrorAttributes((ServerRequest) request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE));
+    }
+}
